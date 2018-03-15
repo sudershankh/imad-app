33ce9054aa62289bcbc1c2805adfe549fa4a22c0
sudershankh <33722188+sudershankh@users.noreply.github.com>
2018-03-15 12:33:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 app.get('/article1', function (req, res) {
   res.sendFile('1');
 });
-app.get('article2', function (req, res) {
+app.get('/article2', function (req, res) {
   res.sendFile('2');
 });
 app.get('/article3', function (req, res) {
